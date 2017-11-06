4c6dba49523d8b9fd511950716df0097d64221fa
SoheeLee <sohee377@gmail.com>
2017-11-07 02:35:47 +0900

    Add django default user model

--- backend/adfreereview/urls.py
+++ backend/adfreereview/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url, include
 from .views import myModelList
+from .views import signup, signin, signout
 
 
 urlpatterns = [
     url(r'^mymodel$', myModelList, name='myModelList'),
+    url(r'^signup$', signup, name='signup'),
+    url(r'^signin$', signin, name='signin'),
+    url(r'^signout$', signout, name='signout'),
 ]
--- backend/adfreereview/views.py
+++ backend/adfreereview/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse, HttpResponseNotAllowed, HttpResponseNotFound, JsonResponse
 from django.views.decorators.csrf import ensure_csrf_cookie
 from .models import MyModel
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 import json
 
 # Create your views here.
@@ -9,5 +11,39 @@ import json
 def myModelList(request):
     if request.method == 'GET':
         return JsonResponse(list(MyModel.objects.all().values()), safe=False)
+    else:
+        return HttpResponseNotAllowed(['GET'])
+
+
+def signup(request):
+    if request.method == 'POST':
+        req_data = json.loads(request.body.decode())
+        username = req_data['username']
+        password = req_data['password']
+        User.objects.create_user(username=username, password=password)
+        return HttpResponse(status=201)
+    else:
+        return HttpResponseNotAllowed(['POST'])
+
+
+def signin(request):
+    if request.method == 'POST':
+        req_data = json.loads(request.body.decode())
+        username = req_data['username']
+        password = req_data['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponse(status=200)
+        else:
+            return HttpResponse(status=401)
+    else:
+        return HttpResponseNotAllowed(['POST'])
+
+
+def signout(request):
+    if request.method == 'GET':
+        logout(request)
+        return HttpResponse(status=200)
     else:
         return HttpResponseNotAllowed(['GET'])
