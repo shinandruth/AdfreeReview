761924b4c77bdfe1ea372a32b0c5e6e43bcea9c5
SoheeLee <sohee377@gmail.com>
2017-11-17 17:16:53 +0900

    Implement django models

--- backend/adfreereview/admin.py
+++ backend/adfreereview/admin.py
@@ -1,6 +1,40 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
 from .models import MyModel
+from .models import Profile
 
 # Register your models here.
 
-admin.site.register(MyModel)
+
+# Define an inline admin descriptor for Profile model
+# which acts a bit like a singleton
+# https://docs.djangoproject.com/en/2.0/topics/auth/customizing/#extending-the-existing-user-model
+# https://simpleisbetterthancomplex.com/tutorial/2016/11/23/how-to-add-user-profile-to-django-admin.html
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+    fk_name = 'user'
+
+
+# Define a new User admin
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline, )
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'get_score')
+    list_select_related = ('profile', )
+
+    def get_score(self, instance):
+        return instance.profile.score
+    get_score.short_description = 'Score'
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
+
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
