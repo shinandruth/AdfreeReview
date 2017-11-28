da20087125ea6ad1a19f1e0fcdc40207a91ef56e
quakkk <saeunsaeun@naver.com>
2017-11-28 21:10:28 +0900

    add post.*, rating.* and implement latest ratings

---
+++ frontend/src/app/post.ts
@@ -0,0 +1,11 @@
+export class Post {
+	id: number;
+	blog_id: number;
+	title: string;
+	url: string;
+	category: string;
+	adfree_score: number;
+	content_score: number;
+	total_score: number;
+	rating_count: number;
+}
---
+++ frontend/src/app/rating.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
+import { Rating } from './rating';
+
+@Injectable()
+export class RatingService {
+  private mainUrl = '/api/main'; 
+  private headers = new Headers({'Content-Type': 'application/json'});
+
+  constructor(private http: Http) { }
+
+  get_latest_ratings(): Promise<Rating[]> {
+
+	return this.http.get(this.mainUrl)
+				.toPromise()
+				.then(response => response.json() as Rating[])
+				.catch(this.handleError);
+  }
+
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error);
+    return Promise.reject(error.message || error);
+  }
+
+}
