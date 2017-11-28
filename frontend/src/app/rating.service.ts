207d8602253fe569b692a5d40d7689a0826772fa
quakkk <saeunsaeun@naver.com>
2017-11-28 22:30:53 +0900

    edit url.py and add user_id in rating.ts

--- frontend/src/app/rating.service.ts
+++ frontend/src/app/rating.service.ts
@@ -7,14 +7,14 @@ import { Rating } from './rating';
 
 @Injectable()
 export class RatingService {
-  private mainUrl = '/api/main'; 
+  private latestUrl = '/api/post/latest'; 
   private headers = new Headers({'Content-Type': 'application/json'});
 
   constructor(private http: Http) { }
 
   get_latest_ratings(): Promise<Rating[]> {
 
-	return this.http.get(this.mainUrl)
+	return this.http.get(this.latestUrl)
 				.toPromise()
 				.then(response => response.json() as Rating[])
 				.catch(this.handleError);
--- frontend/src/app/rating.ts
+++ frontend/src/app/rating.ts
@@ -1,6 +1,7 @@
 export class Rating {
 	id: number;
 	post_id: number;
+	user_id: number;
 	time_stamp : string;
 	adfree_score: number;
 	content_score: number;
