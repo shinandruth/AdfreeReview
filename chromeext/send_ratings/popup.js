ff5df0f4c6c8fd5dddaff5a0d61ead4aa93b6b6d
shinandruth <lleeshin@gmail.com>
2017-12-01 17:22:49 +0900

    fixing merge conflict

--- chromeext/send_ratings/popup.js
+++ chromeext/send_ratings/popup.js
@@ -17,40 +17,53 @@ function create_rating(e){
 
   chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT}, function (tabs) {
     url = tabs[0].url;
-    xhr.open("POST", base_url, true);  // FIXME localhost
+    xhr.open("POST", base_url, true); // FIXME localhost
     xhr.setRequestHeader("Content-type", "application/json");
-    json_rating = JSON.stringify({adfreescore: adfreescore, contentscore: contentscore, comment:comment, url:url});
+    json_rating = JSON.stringify({
+      adfreescore: adfreescore,
+      contentscore: contentscore,
+      comment: comment,
+      url: url
+    });
     xhr.send(json_rating);
   });
   //window.close();
  }
 
-function sign_up(e){
-  chrome.tabs.create({url: "http://localhost:4200/welcome"});
+function sign_up(e) {
+  chrome.tabs.create({
+    url: "http://localhost:4200/welcome"
+  });
 }
 
-function duplicate_current_tab(e){
-  chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT}, function (tabs) {
+function duplicate_current_tab(e) {
+  chrome.tabs.query({
+    'active': true,
+    'windowId': chrome.windows.WINDOW_ID_CURRENT
+  }, function(tabs) {
     url = tabs[0].url;
-    chrome.tabs.create({url: url});
+    chrome.tabs.create({
+      url: url
+    });
   })
 }
 
 function get_post_score(){
   var url;
-  chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT}, function (tabs) {
+  chrome.tabs.query({
+    'active': true,
+    'windowId': chrome.windows.WINDOW_ID_CURRENT
+  }, function(tabs) {
     url = tabs[0].url;
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "http://localhost:8000/api/score/" + url, false);
     var scores;
-    xhr.onload = function (){
-      if(xhr.readyState == 4 && xhr.status == "200"){
+    xhr.onload = function() {
+      if (xhr.readyState == 4 && xhr.status == "200") {
         scores = JSON.parse(xhr.responseText);
-      }
-      else if(xhr.status == "404"){
+      } else if (xhr.status == "404") {
         window.alert("No one evaluate this post yet. Please be the first one!");
-      }
-      else{
+      } else {
         window.alert("Error: fail to get score of this post")
       }
     }
