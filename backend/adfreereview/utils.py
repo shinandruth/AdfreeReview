082a2be8e984d1c37be984f2d18a947df733bdfc
EC2 Default User <ec2-user@ip-172-31-12-187.ap-northeast-2.compute.internal>
2017-12-08 06:02:57 +0000

    allowing comment as None

--- backend/adfreereview/utils.py
+++ backend/adfreereview/utils.py
@@ -68,6 +68,8 @@ def check_blog_url(url):
 def check_rating_validity(req_data):
     for component, match in rating_infos.items():
         if not match.match(req_data[component]):
+            if component == 'comment' and req_data[component] == '':
+                continue
             return (False, component)
     return (True, None)
 
