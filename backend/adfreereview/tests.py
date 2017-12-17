4d0a4979f7be759d6f90d10d8b582fbcd7b9411e
shinandruth <lleeshin@gmail.com>
2017-12-17 20:13:19 +0900

    fixed chrome textbox ailgnment

--- backend/adfreereview/tests.py
+++ backend/adfreereview/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.forms.models import model_to_dict
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from .models import Blog, Post, Rating, Profile
+
+class AdfreeTestCase(TestCase):
+    def setUp(self):
+        User.objects.create(username='admin', password='adminadmin', email="a@a.com")
+
+    def test_user_str(self):
+        admin = User.objects.get(username='admin')
+        self.assertEqual(str(admin), 'admin')
